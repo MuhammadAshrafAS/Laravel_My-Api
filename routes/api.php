5b37d041d26d9a4b6cef67dcdb7d60ed6f2fc235
MuhammadAshrafAS <carcassbarker@gmail.com>
2026-03-17 13:28:54 +0700

    6.3 Proteksi Route (Middleware)

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::apiResource('/categories', CategoryController::class);
 Route::apiResource('/products', ProductController::class);
+
+// Route Public (Bisa diakses siapa saja)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Route Private (Harus bawa Token)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // CRUD Produk & Kategori kita masukkan sini
+    Route::apiResource('/categories', CategoryController::class);
+    Route::apiResource('/products', ProductController::class);
+});

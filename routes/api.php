f9bdae38970c539f649a0c0f6556f79de3457056
MuhammadAshrafAS <carcassbarker@gmail.com>
2026-03-17 13:16:22 +0700

    5.3 Controller Produk dengan Relasi

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/products', ProductController::class);
